829ebda55dc8ab67caaaa94ffcd23d9e8346ade0
yugengde <yugengde@163.com>
2019-08-03 17:10:15 +0800

    modify README.md

--- README.md
+++ README.md
@@ -24,3 +24,33 @@ $> python3 train.py
 ```
 
 
+
+downloading chinese scene text recognition training data and data preparation
+=======
+
+- label
+
+	[train.list](http://bj.bcebos.com/v1/ai-studio-online/4edf40b576534bc4b6d6d25c7ac30325ba5d9302c9da4316a04c865cee1e92bd?responseContentDisposition=attachment%3B%20filename%3Dtrain.list&authorization=bce-auth-v1%2F0ef6765c1e494918bc0d4c3ca3e5c6d1%2F2019-06-21T09%3A47%3A06Z%2F-1%2F%2F19d07e6fb96fc15811fb072ef9804aeb1dbb633dfb0d10eab78003c00b648122)
+
+- training data
+
+	[train_images.tar.gz](http://bj.bcebos.com/v1/ai-studio-online/f9328e4264514b69bd85a65bc7ec6623ac7f8feca87a496993215c01f7bc3778?responseContentDisposition=attachment%3B%20filename%3Dtrain_images.tar.gz&authorization=bce-auth-v1%2F0ef6765c1e494918bc0d4c3ca3e5c6d1%2F2019-06-21T09%3A47%3A46Z%2F-1%2F%2Fec807378f4011e3a1a969393ec4541211cb34d2a46b7778f429808028b75e863)
+    
+- put your download data in your path
+
+
+    
+data preparation    
+=======
+
+1. extract image file
+```sh
+tar -xvf train_images.tar.gz -C /path/to/<your repositories>
+```
+
+2. indexing image
+```python
+pyton3 indexing.py
+```
+
+
---
+++ indexing.py
@@ -0,0 +1,46 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import  pickle
+
+
+def indexing(filename='char2index.pkl'):
+
+    char2index = {}
+
+    with open('train.list', 'r') as f:
+        char2num = {}
+        for line in f.readlines():
+            chars = line.split()[-1]
+            if chars:
+                for char in chars:
+                    if not char2num.get(char):
+                        char2num[char] = 1
+                    else:
+                        char2num[char] += 1
+
+        sort_char2num = sorted(char2num.items(), key=lambda item: item[1], reverse=True)
+        
+        for index, (char,num) in enumerate(sort_char2num):
+            # print(index, char, num)
+            char2index[char] = index
+
+        with  open(filename, 'wb') as p:
+            pickle.dump(char2index, p)
+
+
+def load_index(filename='char2index.pkl'):
+    with open(filename, 'rb') as f:
+        char2index = pickle.load(f)
+
+        return char2index
+
+
+if __name__ == "__main__":
+
+    filename = './char2index.pkl'
+
+    indexing(filename)
+    char2index = load_index(filename)
+    print(char2index)
+
